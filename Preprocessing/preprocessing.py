286d4806e2dcbe4f1c51ab2acb0e1f5233463d03
Alessia Finiguerra <alefini01@gmail.com>
2025-06-11 13:15:02 +0200

    aggiornata data_exploration

--- Preprocessing/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-pd.set_option('display.max_columns', None)  # visualizza tutte le colonne del dataset
-
-# Caricamento del dataset
-data = pd.read_csv('./Dataset/personality_dataset.csv')
-
-# Visualizzazione delle prime righe del dataset e delle informazioni generali
-print(data.head())

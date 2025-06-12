ed84b048cb8823efe9b3e978e7129eb704e90089
Alessia Finiguerra <alefini01@gmail.com>
2025-06-12 13:13:32 +0200

    completata data_exploration

--- Apprendimento supervisionato/data_exploration.py
+++ Apprendimento supervisionato/data_exploration.py
@@ -30,48 +30,49 @@ print(numerical_columns)
 print("\n-----Colonne categoriche-----")
 print(categorical_columns)
 
-print("\n-----Visualizzazione dei valori delle features-----")
-# visualizzazione features numeriche
-plt.figure(figsize=(10, 7))
-for i, col in enumerate(numerical_columns, 1):
-    plt.subplot(3, 2, i)
-    sns.histplot(data=df, x=col, bins=15, kde=True, color="skyblue")
-    plt.title(f'{col}')
-    plt.xlabel("")
-    plt.ylabel("Frequenza")
-plt.tight_layout(pad=3.0)
-plt.show()
+if __name__ == "__main__":
+    print("\n-----Visualizzazione dei valori delle features-----")
+    # visualizzazione features numeriche
+    plt.figure(figsize=(10, 7))
+    for i, col in enumerate(numerical_columns, 1):
+        plt.subplot(3, 2, i)
+        sns.histplot(data=df, x=col, bins=15, kde=True, color="skyblue")
+        plt.title(f'{col}')
+        plt.xlabel("")
+        plt.ylabel("Frequenza")
+    plt.tight_layout(pad=3.0)
+    plt.show()
 
-# visualizzazione delle features categoriche
-plt.figure(figsize=(10, 7))
-for i, col in enumerate(categorical_columns, 1):
-    plt.subplot(2, 2, i)
-    sns.countplot(data=df, x=col, palette="pastel")
-    plt.title(f'{col}')
-    plt.xlabel("")
-    plt.ylabel("Conteggio")
-    plt.xticks(rotation=45)
-plt.tight_layout(pad=3.0)
-plt.show()
+    # visualizzazione delle features categoriche
+    plt.figure(figsize=(10, 7))
+    for i, col in enumerate(categorical_columns, 1):
+        plt.subplot(2, 2, i)
+        sns.countplot(data=df, x=col, palette="pastel")
+        plt.title(f'{col}')
+        plt.xlabel("")
+        plt.ylabel("Conteggio")
+        plt.xticks(rotation=45)
+    plt.tight_layout(pad=3.0)
+    plt.show()
 
-print("\n-----Visualizzazione della correlazione tra le features e il target-----")
-# feature numeriche con il target
-plt.figure(figsize=(15, 7))
-for i, col in enumerate(numerical_columns, 1):
-    plt.subplot(2, 3, i)
-    sns.boxplot(x='Personality', y=col, data=df, palette='Set2')
-    plt.title(f'{col} by Personality')
-    plt.xticks(rotation=45)
-plt.tight_layout(pad=3.0)
-plt.show()
+    print("\n-----Visualizzazione della correlazione tra le features e il target-----")
+    # feature numeriche con il target
+    plt.figure(figsize=(15, 7))
+    for i, col in enumerate(numerical_columns, 1):
+        plt.subplot(2, 3, i)
+        sns.boxplot(x='Personality', y=col, data=df, palette='Set2')
+        plt.title(f'{col} by Personality')
+        plt.xticks(rotation=45)
+    plt.tight_layout(pad=3.0)
+    plt.show()
 
-# feature categoriche con il target
-cat_col = [col for col in categorical_columns if col != 'Personality']
-plt.figure(figsize=(10, 6))
-for i, col in enumerate(cat_col, 1):
-    plt.subplot(1, 2, i)
-    sns.countplot(x=col, hue='Personality', data=df, palette='Set1')
-    plt.title(f'{col} by Personality')
-    plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+    # feature categoriche con il target
+    cat_col = [col for col in categorical_columns if col != 'Personality']
+    plt.figure(figsize=(10, 6))
+    for i, col in enumerate(cat_col, 1):
+        plt.subplot(1, 2, i)
+        sns.countplot(x=col, hue='Personality', data=df, palette='Set1')
+        plt.title(f'{col} by Personality')
+        plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()

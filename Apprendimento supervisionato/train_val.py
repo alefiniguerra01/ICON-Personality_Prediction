5c0ad8ece6c5039c1831056334e527ebc5133414
Alessia Finiguerra <alefini01@gmail.com>
2025-06-13 18:29:42 +0200

    aggiunte matrici di confusione provvisorie

--- Apprendimento supervisionato/train_val.py
+++ Apprendimento supervisionato/train_val.py
@@ -6,7 +6,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
-import numpy as np
+
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Importazione del DataFrame preprocessato
 
 # Suddivisione tra features e target
 X = df.drop("Personality", axis=1)
@@ -33,7 +37,20 @@ for name, model in models.items():
     model.fit(X_train_scaled, y_train)
     y_preds = model.predict(X_test_scaled)
     acc = accuracy_score(y_test, y_preds)
-    print(f"\n-----{name}: {acc:.3f}-----")
+    print(f"\n-----{name}= Accuracy: {acc:.3f}-----")
     print("\n Classification Report:\n", classification_report(y_test, y_preds, target_names=['Extrovert', 'Introvert']))
 
     print("------------------")
+
+    # DA ELIMINARE
+    if __name__ == "__main__":
+        cm = confusion_matrix(y_test, y_preds)
+
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
+                    xticklabels=['Extrovert', 'Introvert'], 
+                    yticklabels=['Extrovert', 'Introvert'])
+        plt.xlabel('Valore Previsto')
+        plt.ylabel('Valore Reale')
+        plt.title(f'Matrice di confusione per il modello {name}')
+        plt.show()

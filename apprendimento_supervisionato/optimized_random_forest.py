662738e8af48bcef2f0f1d12523b548288186568
Alessia Finiguerra <alefini01@gmail.com>
2025-07-22 18:48:20 +0200

    sistemata ottimizzazione random forest

--- apprendimento_supervisionato/optimized_random_forest.py
+++ apprendimento_supervisionato/optimized_random_forest.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
-import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.preprocessing import StandardScaler
 from scipy.stats import randint
 from preprocessing import df
 
